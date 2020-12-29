7124688f2f95316953d3041d7a80fbc7d28508f9
DVP97 <45999557+DVP97@users.noreply.github.com>
2020-12-29 23:46:21 +0100

    fuentes y estilo

--- contenido.php
+++ contenido.php
@@ -5,11 +5,14 @@ require('db.php');
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!--Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="css/textStyle.css">
+    <!--titulo-->
     <title>SW - Timeline</title>
+    <!--logo tab navegador-->
     <link rel="icon" type="image/png" href="media/Fulcrum.png">
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body background="media/background.jpg">
     <header>
@@ -21,27 +24,37 @@ require('db.php');
         </nav>
     </header>
     <?php
+    //falta crear constructor sentencia
     $sel_query="SELECT * FROM  Contenido WHERE IDcontenido = 1;";
     $result = mysqli_query($con,$sel_query);
     while($row = mysqli_fetch_assoc($result)) { 
     ?>
-    <!--falta añadir portada-->
-    <div class="container">
-        <div class="row">
-            <div class="col"><!--portada--><img src="media/Fulcrum.png" alt="icon" height="300px"></div>
-            <div class="col">
-                <div class="row">
-                    <div class="col-md-7"><?php echo $row["Name"]; ?></div>
-                    <div class="col-md-3">aurebesh translation</div>
+
+    <!--Columna Portada-->
+    <div class="row">
+        <div class="col-xs-6 col-md-4">
+            <img src="media/Fulcrum.png" alt="icon">
+        </div>        
+        <!--Columna Informacion-->
+        <div class="col-xs-6 col-md-8">
+            <div class="row">
+                <div class="col-xs-6 col-md-9">
+                    <h1 class="title" style="color:#FFE81F";><?php echo utf8_decode($row["Name"]); ?></h1>
                 </div>
-                <div class="row">
-                    <div class="col-md-3"><?php echo $row["Release"]; ?></div>
-                    <div class="col-md-2"><?php echo $row["Type"]; ?></div>
-                    <div class="col-md-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">
+                    <h3 style="color:#FFE81F; font-size:20px; ">aurebesh translation</h3>
+                    <h3 style="color:#fdfbec; font-size:15px; "><?php echo $row["Release"]; ?></h3>
+                    <h3 style="color:#fdfbec; font-size:15px; "><?php echo $row["Type"]; ?> - Plataformas </h3>
                 </div>
-                <div class="row">
-                    <div class="col-md-7"><?php echo $row["Info"]; ?></div>
-                    <div class="col">Insertar trailer</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-6">
+                    <h3 style="color:azure; font-size:20px; text-align:justify"><?php echo $row["Info"]; ?></h3>
+                </div>
+                <div class="col-md-6">
+                    Insertar Trailer 
                 </div>
             </div>
         </div>
