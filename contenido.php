5c287a03941ce2c2301e3dff308d51e5aa56fbaa
DVP97 <45999557+DVP97@users.noreply.github.com>
2020-12-30 15:05:30 +0100

    yo q se

--- contenido.php
+++ contenido.php
@@ -4,25 +4,26 @@ require('db.php');
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <!--Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="css/textStyle.css">
-    <!--titulo-->
-    <title>SW - Timeline</title>
-    <!--logo tab navegador-->
-    <link rel="icon" type="image/png" href="media/Fulcrum.png">
+        <meta charset="UTF-8">
+        <!--Custom CSS-->
+        <link rel="stylesheet" href="css/style.css">
+        <title>SW - Timeline</title>
+        <!-- Bootstrap4 CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+        <link rel="icon" type="image/png" href="media/Fulcrum.png">
 </head>
 <body background="media/background.jpg">
-    <header>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html"><img src="media/Fulcrum.png" alt="icon" height="50"></a>
-            <a class="navbar-brand" href="#">Star Wars Saga Timeline</a>
-            <a class="button" href="login.html">Login</a>
-            <a class="button" href="Register.html">Register</a>
-        </nav>
-    </header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="index.html"><img src="media/Fulcrum.png" alt="icon" height="50">Star Wars Saga Timeline</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu"><span class="navbar-toggler-icon"></span></button>
+        <div id="navbarMenu" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="Register.html">Register</a></li>
+            </ul>
+        </div>
+    </nav>
     <?php
     //falta crear constructor sentencia
     $sel_query="SELECT * FROM  Contenido WHERE IDcontenido = 1;";
@@ -33,36 +34,44 @@ require('db.php');
     <div id="contenedor" class="container-fluid main-wrapper"
         <!--Columna Portada-->
         <div class="row no-gutters">
-            <div class="col">
-                <img src="media/Fulcrum.png" alt="icon" style="height:80vh">
+            <div class="col-md-3 col-xs-2">
+                <img src="media/Fulcrum.png" alt="icon" style="max-height:700px">
             </div> 
             
             <!--Columna Informacion-->
             <div class="col">
                 <div class="row">
-                    <div class="col-xs-6 col-md-9">
+                    <div class="col-xs-6 col-sm-6 col-md-9">
                         <h1 class="title" style="color:#FFE81F";><?php echo utf8_decode($row["Name"]); ?></h1>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-12 col-md-4">
+                    <div class="col-xs-6 col-md-4">
                         <h3 style="color:#FFE81F; font-size:20px; ">aurebesh translation</h3>
                         <h3 style="color:#fdfbec; font-size:15px; "><?php echo $row["Release"]; ?></h3>
                         <h3 style="color:#fdfbec; font-size:15px; "><?php echo $row["Type"]; ?> - Plataformas </h3>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-12 col-md-6">
+                    <div class="col-xs-6">
                         <h3 style="color:azure; font-size:20px; text-align:justify"><?php echo $row["Info"]; ?></h3>
                     </div>
-                    <div class="col-md-6">
-                    <iframe src="<?php echo $row["trailer"]; ?>" style="width:50vh"></iframe> 
-                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-offset-1"><iframe src="<?php echo $row["trailer"]; ?>"></iframe></div>
                 </div>
             </div>
+            
         </div>
     </div>
+    <div class="push"></div>
+    <footer class="footer">
+        <hr><br><center><div class="container"><span class="text-muted">Copyright disclaimer: all rigths belong to Lucasfilm and associates.</span></div></center>
+    </footer>
     <?php }?>
-
+    <!--Bootstrap4 JS-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
