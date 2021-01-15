7e5e8c010672ca0de1aface51d975be0af045ca1
DVP97 <45999557+DVP97@users.noreply.github.com>
2021-01-15 13:54:01 +0100

    hotfix register.php
    solo quería poner hotfix porque suena to chulo

--- register.php
+++ register.php
@@ -30,12 +30,12 @@ if (isset($_REQUEST['username'])){
  $password = stripslashes($_REQUEST['password']);
  $password = mysqli_real_escape_string($con,$password);
         $query = "INSERT into `users` (username, email, password)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+VALUES ('$username','$email','".md5($password)."')";
         $result = mysqli_query($con,$query);
         if($result){
             echo "<div class='form'>
 <h3>You are registered successfully.</h3>
-<br/>Click here to <a href='login.html'>Login</a></div>";
+<br/>Click here to <a href='login.php'>Login</a></div>";
         }
     }else{
 ?>
